2e085ca0a94a91ddf1cfd39a004c2328c72c28b1
Creighton <smoothdestiny90@gmail.com>
2026-05-29 13:04:44 -0700

    Update bitwise_xor.sv
    Added project header to file. Changed output y to result to match other files. Fixed module name to correct function.

--- bitwise_xor.sv
+++ bitwise_xor.sv
@@ -1,8 +1,18 @@
-module bitwise_and(
+/*
+ * Project: 8-bit ALU
+ * Author: Creighton Townsley, Wakana Sakane, Trevor Bahm
+ * Contact: townslcr@oregonstate.edu
+ * Description: Bitwise XOR module for 8-bit ALU.
+ *              Uses combinational logic to perform bitwise XOR operation for two 8-bit numbers.
+ * Sources: ECE 204 course materials
+ * Date: May 2026
+ */
+
+module bitwise_XOR(
 	input logic [7:0] a,
 	input logic [7:0] b,
-	output logic [7:0] y
+	output logic [7:0] result
 );
-	assign y = a ^ b;
+	assign result = a ^ b;
 
 endmodule
