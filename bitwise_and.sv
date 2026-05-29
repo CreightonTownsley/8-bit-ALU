8f8bffb12d5223e07faf08280c2f0c03782bc7ae
Creighton <smoothdestiny90@gmail.com>
2026-05-29 13:00:03 -0700

    Update bitwise_and.sv
    Added project header to file, updated output y to result to match other files.

--- bitwise_and.sv
+++ bitwise_and.sv
@@ -1,8 +1,17 @@
-module bitwise_and(
-	input logic [7:0] a,
-	input logic [7:0] b,
-	output logic [7:0] y
-);
-	assign y = a & b;
+/*
+ * Project: 8-bit ALU
+ * Author: Creighton Townsley, Wakana Sakane, Trevor Bahm
+ * Contact: townslcr@oregonstate.edu
+ * Description: Bitwise AND module for 8-bit ALU.
+ *              Uses combinational logic to perform bitwise AND operation for two 8-bit numbers.
+ * Sources: ECE 204 course materials
+ * Date: May 2026
+ */
 
+module bitwise_and(
+    input  logic [7:0] a,
+    input  logic [7:0] b,
+    output logic [7:0] result
+);
+    assign result = a & b;
 endmodule
