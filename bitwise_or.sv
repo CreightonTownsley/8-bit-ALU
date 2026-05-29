6d027924ad9006c65ecd74c1628f07f0851bf294
Creighton <smoothdestiny90@gmail.com>
2026-05-29 13:03:10 -0700

    Update bitwise_or.sv
    Added project header to file. Changed output y to result to match other files. Fixed module name to correct function.

--- bitwise_or.sv
+++ bitwise_or.sv
@@ -1,8 +1,18 @@
-module bitwise_and(
+/*
+ * Project: 8-bit ALU
+ * Author: Creighton Townsley, Wakana Sakane, Trevor Bahm
+ * Contact: townslcr@oregonstate.edu
+ * Description: Bitwise OR module for 8-bit ALU.
+ *              Uses combinational logic to perform bitwise OR operation for two 8-bit numbers.
+ * Sources: ECE 204 course materials
+ * Date: May 2026
+ */
+
+module bitwise_or(
 	input logic [7:0] a,
 	input logic [7:0] b,
-	output logic [7:0] y
+	output logic [7:0] result
 );
-	assign y = a | b;
+	assign result = a | b;
 
 endmodule
