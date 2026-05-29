64feb38345c742ac797fcf050e12dca60ea17b94
Creighton <smoothdestiny90@gmail.com>
2026-05-29 13:07:10 -0700

    Update outputA.sv
    Added project header to file. Removed unnecessary input b. Changed output y to result to match other files.

--- outputA.sv
+++ outputA.sv
@@ -1,8 +1,16 @@
-module outputA(
-	input logic [7:0] a,
-	input logic [7:0] b,
-	output logic [7:0] y
-);
-	assign y = a;
+/*
+ * Project: 8-bit ALU
+ * Author: Creighton Townsley, Wakana Sakane, Trevor Bahm
+ * Contact: townslcr@oregonstate.edu
+ * Description: Pass A module for 8-bit ALU.
+ *              Uses combinational logic to pass A input through to output.
+ * Sources: ECE 204 course materials
+ * Date: May 2026
+ */
 
+module outputA(
+    input  logic [7:0] a,
+    output logic [7:0] result
+);
+    assign result = a;
 endmodule
