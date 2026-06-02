bcd9783dfa74631282b7e9e39a85b0ebc1aeccb1
md8rsdhv9v-stack <md8rsdhv9v@privaterelay.appleid.com>
2026-06-02 09:19:28 -0700

    output_handler.sv

--- output_handler.sv
+++ output_handler.sv
@@ -1,12 +1,12 @@
 module output_handler (
     input  logic [2:0] Opcode,
     input  logic       Enable,
-    input  logic [7:0] add_sub_res,
-    input  logic [7:0] and_res,
-    input  logic [7:0] or_res,
-    input  logic [7:0] xor_res,
-    input  logic [7:0] outA_res,
-    input  logic [7:0] outB_res,
+    input  logic [7:0] add_sub_result,
+    input  logic [7:0] and_result,
+    input  logic [7:0] or_result,
+    input  logic [7:0] xor_result,
+    input  logic [7:0] outA_result,
+    input  logic [7:0] outB_result,
    
     output logic [7:0] Result
 );
@@ -21,13 +21,13 @@ module output_handler (
         else begin
             
             case (Opcode)
-                3'b000: Result = add_sub_res; // Add
-                3'b001: Result = add_sub_res; // Sub
-                3'b010: Result = and_res;  
-                3'b011: Result = or_res;      
-                3'b100: Result = xor_res;     
-                3'b101: Result = outA_res;    
-                3'b110: Result = outB_res; 
+                3'b000: Result = add_sub_result; // Add
+                3'b001: Result = add_sub_result; // Sub
+                3'b010: Result = and_result;  
+                3'b011: Result = or_result;      
+                3'b100: Result = xor_result;     
+                3'b101: Result = outA_result;    
+                3'b110: Result = outB_result; 
                 default: Result = 8'b00000000; 
             endcase
         end
